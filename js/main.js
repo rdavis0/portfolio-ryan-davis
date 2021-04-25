dce42a39b20e86c69ee1508f5c3d8f7a5589924a
rdavis0 <rdfiasco@gmail.com>
2021-04-24 22:50:01 -0600

    Add table of contents

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const links = [
+    {
+        label: "Week 1 Notes",
+        url: "week1/index.html"
+    }
+]
+
+function generateContents() {
+    var list = document.getElementById("table-of-contents");
+    for (i in links) {
+        var node = document.createElement("li");
+        var anchor = document.createElement("a");
+        anchor.setAttribute("href", links[i].url);
+        anchor.innerText = links[i].label;
+        node.appendChild(anchor);
+        list.appendChild(node);
+    };
+}
+
+generateContents();
